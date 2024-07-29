a751fd41c40dae6d49cad7f4357c89ac734e076a
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-29 21:06:53 +0200

    How good are you really_done

--- How good are you really/How good are you really.js
+++ How good are you really/How good are you really.js
@@ -9,3 +9,14 @@
 
 
 // Solution:
+
+function betterThanAverage(classPoints, yourPoints) {
+    let sum = 0;
+    for (let i = 0; i < classPoints.length; i++) {
+      sum += classPoints[i];
+    }
+    let classAvg = sum / classPoints.length;
+    return yourPoints > classAvg;
+  
+  }
+  
