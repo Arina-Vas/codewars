d52dc2f854b963bc091b880000d3e601494eb6ba
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 15:31:32 +0200

    Vowel Count_done

--- 15_Vowel Count/Vowel Count.js
+++ 15_Vowel Count/Vowel Count.js
@@ -6,3 +6,12 @@
 // The input string will only consist of lower case letters and/or spaces.
 
 // Solution:
+function getCount(str) {
+    let vow = /[aeiou]/gi;
+     let num = str.match(vow);
+    if (num === null) {return 0;}
+     else { return num.length;}
+  
+  }
+  console.log(getCount("pear tree"));
+  console.log(getCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli")); 
