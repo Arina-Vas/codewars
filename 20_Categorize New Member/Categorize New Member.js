a866037d5aa006821d95dc17bc02b21bfb4b9fe3
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 18:24:50 +0200

    Categorize New Member_done

--- 20_Categorize New Member/Categorize New Member.js
+++ 20_Categorize New Member/Categorize New Member.js
@@ -14,3 +14,19 @@
 // output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 
 // Solution:
+function openOrSenior(data){
+    let res = [];
+    let result = [];
+    for (let i = 0; i < data.length; i++) {
+
+    if (data[i][0] >= 55 && data[i][1] > 7)
+      {
+        result = res.push('Senior');
+      }
+    else {
+        result = res.push('Open');
+    }
+  }
+  return res;
+}
+  console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]));
