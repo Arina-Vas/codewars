93d669bf55a6b2bdfc081df396d229925eb35039
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 15:15:18 +0200

    Rotate for a Max_done

--- 12_Rotate for a Max/Rotate for a Max.js
+++ 12_Rotate for a Max/Rotate for a Max.js
@@ -25,3 +25,18 @@
 // max_rot(38458215) should return 85821534
 
 // Solution:
+
+function maxRot(n) {
+    let array = String(n).split(''),
+        result = n
+    for (let i = 0; i < array.length - 1; i++) {
+        array.push(array.splice(i, 1))
+        let num = Number(array.join(''))
+        if (num > result) {
+            result = num
+        }
+    }
+    return result
+}
+console.log(maxRot(38458215));
+console.log(maxRot(257117280));
