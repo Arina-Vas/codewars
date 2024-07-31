f5b74de3cc550c9ba63af57e53aee6884e2a3ae0
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 17:19:32 +0200

    Two to One_done

--- 18_Two to One/Two to One.js
+++ 18_Two to One/Two to One.js
@@ -11,3 +11,14 @@
 // longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 
 // Solution:
+function longest(s1, s2) {
+    let res = "";
+    let abc = 'abcdefghijklmnopqrstuvwxyz'
+    for (let i = 0; i < abc.length; i++) {
+        if (s1.includes(abc[i]) || s2.includes(abc[i])) {
+            res += abc[i];
+        }
+    }
+    return res;
+}
+console.log(longest("xyaabbbccccdefww", "xxxxyyyyabklmopq"));
