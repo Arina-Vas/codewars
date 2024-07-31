8d6d943f76b032364f618f03d2aa92afa5ea97d7
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 12:39:50 +0200

    Grasshopper - Summation_done

--- Grasshopper - Summation/Grasshopper - Summation.js
+++ Grasshopper - Summation/Grasshopper - Summation.js
@@ -7,4 +7,13 @@
 // 2 -> 3 (1 + 2)
 // 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
 
-// Solution
+// Solution:
+var summation = function (num) {
+    let sum = 0;
+
+    for (let i = 1; i <= num; i++) {
+        sum += i;
+    }
+
+    return sum;
+}
