e88d7f37f406cb076198dbfbfd7b11c41ac29329
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 13:36:41 +0200

    Super Duper Easy_done

--- 10_Super Duper Easy/Super Duper Easy.js
+++ 10_Super Duper Easy/Super Duper Easy.js
@@ -2,3 +2,15 @@
 // Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
 
 // Solution:
+function problem(x) {
+    if (typeof x === "string") {
+        return "Error"
+
+    }
+    else {
+        return (x * 50) + 6;
+    }
+}
+
+console.log(problem("hello"));
+console.log(problem(5));
