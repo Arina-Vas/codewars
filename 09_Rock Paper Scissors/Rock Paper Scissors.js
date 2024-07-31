56b17dc91d95c303e2eb947b853137f3c89c3f38
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 13:05:35 +0200

    Rock Paper Scissors_done

--- 09_Rock Paper Scissors/Rock Paper Scissors.js
+++ 09_Rock Paper Scissors/Rock Paper Scissors.js
@@ -9,3 +9,25 @@
 // "paper", "paper" --> "Draw!"
 
 // Solution:
+
+const rps = (p1, p2) => {
+    if (p1 === p2) {
+        return 'Draw!'
+    } if (p1 === 'scissors' && p2 === 'paper') {
+        return 'Player 1 won!'
+    } if (p1 === 'scissors' && p2 === 'rock') {
+        return 'Player 2 won!'
+    } if (p1 === 'rock' && p2 === 'paper') {
+        return 'Player 2 won!'
+    } if (p1 === 'rock' && p2 === 'scissors') {
+        return 'Player 1 won!'
+    } if (p1 === 'paper' && p2 === 'scissors') {
+        return 'Player 2 won!'
+    } if (p1 === 'paper' && p2 === 'rock') {
+        return 'Player 1 won!'
+    }
+};
+
+console.log(rps('rock', 'scissors'));
+console.log(rps('scissors', 'scissors'));
+console.log(rps('paper', 'scissors'));
