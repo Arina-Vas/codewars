a3c22693005ea90e7095c5c627e2428d09e19882
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 15:35:42 +0200

    Complementary DNA_done

--- 16_Complementary DNA/Complementary DNA.js
+++ 16_Complementary DNA/Complementary DNA.js
@@ -13,3 +13,23 @@
 // "GTAT" --> "CATA"
 
 // Solution:
+function dnaStrand(dna) {
+    dna = dna.split('');
+    for (let i = 0; i < dna.length; i++) {
+        if (dna[i] == 'A') {
+            dna.splice(i, 1, "T");
+        }
+        else if (dna[i] == 'T') {
+            dna.splice(i, 1, "A");
+        }
+        else if (dna[i] == 'C') {
+            dna.splice(i, 1, "G");
+        }
+        else if (dna[i] == 'G') {
+            dna.splice(i, 1, "C");
+        }
+    }
+    return dna.join('');
+}
+console.log(dnaStrand("GTAT"));
+console.log(dnaStrand("ATTGCAAAAATTGCGTATC"));
