6acb69008deb99c8cb3c8a0449c1005b30f759ec
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-07-31 15:18:47 +0200

    Remove exclamation marks_done

--- 13_Remove exclamation marks/Remove exclamation marks.js
+++ 13_Remove exclamation marks/Remove exclamation marks.js
@@ -2,3 +2,9 @@
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 // Solution:
+function removeExclamationMarks(s) {
+    const exc = /!/g;
+    return s.replace(exc, '');
+}
+console.log(removeExclamationMarks("Hello World!!!"));
+console.log(removeExclamationMarks("Hi! Hello!"));
