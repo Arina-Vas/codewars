1d674ecd9354fef716ff544831f5053e39b51fb1
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-08-02 16:38:53 +0200

    Convert boolean value_done

--- 29_Convert boolean values to strings 'Yes' or 'No'/Convert boolean values to strings 'Yes' or 'No'.js
+++ 29_Convert boolean values to strings 'Yes' or 'No'/Convert boolean values to strings 'Yes' or 'No'.js
@@ -2,3 +2,16 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 // Solution:
+
+function boolToWord( bool ){
+    if (bool) {
+      return "Yes";
+    }
+    else if (!bool) {
+      return "No";
+    }
+  }
+
+  console.log(boolToWord(false));
+  console.log(boolToWord(true));
+  console.log(boolToWord(null));
