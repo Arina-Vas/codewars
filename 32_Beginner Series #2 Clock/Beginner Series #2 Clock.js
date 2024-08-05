ff86da3669578fe1838ad08016cacc38381cf921
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-08-05 21:44:10 +0200

    Beginner Series #2 Clock_done

--- 32_Beginner Series #2 Clock/Beginner Series #2 Clock.js
+++ 32_Beginner Series #2 Clock/Beginner Series #2 Clock.js
@@ -16,3 +16,25 @@
 // 0 <= s <= 59
 
 // Solution:
+function past(h, m, s) {
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    if (0 <= h <= 23) {
+        a = h * 60 * 60;
+    }
+    if (0 <= m <= 59) {
+        b = m * 60;
+    }
+    if (0 <= s <= 59) {
+        c = s * 1;
+    }
+    return result = (a + b + c) * 1000;
+
+
+
+
+
+
+}
+console.log(past(0, 1, 1));
