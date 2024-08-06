3ab7c696e18cdf609e4de6f40c1d68e44eb9b86b
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-08-06 11:36:21 +0200

    simpleMultiplication_done

--- 34_Simple multiplication/Simple multiplication.js
+++ 34_Simple multiplication/Simple multiplication.js
@@ -3,3 +3,15 @@
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 // Solution:
+function simpleMultiplication(number) {
+    if (number % 2 === 0) {
+        return number * 8;
+    }
+    else {
+        return number * 9;
+    }
+}
+console.log(simpleMultiplication(7));
+console.log(simpleMultiplication(6));
+console.log(simpleMultiplication(8));
+console.log(simpleMultiplication(5));
