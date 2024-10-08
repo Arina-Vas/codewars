fc79c33581d35862ef7634216d16dddd7db5cbe8
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-10-08 12:34:59 +0200

    07_Array plus array_solution

--- 00_courses/07_Array plus array/main.js
+++ 00_courses/07_Array plus array/main.js
@@ -4,3 +4,14 @@
 // P.S. Each array includes only integer numbers. Output is a number too.
 
 // Solution:
+function arrayPlusArray(arr1, arr2) {
+    let sum = 0;
+    for (let i = 0; i < arr1.length; i++) {
+        sum += arr1[i];
+    }
+    for (let j = 0; j < arr1.length; j++) {
+        sum += arr2[j];
+    }
+    return sum; //something went wrong
+}
+console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]));
