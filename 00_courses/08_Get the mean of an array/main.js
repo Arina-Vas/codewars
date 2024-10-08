70b98f0d7d390a8e54a51737e323469128963623
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-10-08 12:46:16 +0200

    08_Get the mean of an array_solution

--- 00_courses/08_Get the mean of an array/main.js
+++ 00_courses/08_Get the mean of an array/main.js
@@ -6,3 +6,12 @@
 // The array will never be empty.
 
 // Solution:
+function getAverage(marks) {
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+    return Math.floor(sum / marks.length);
+    //TODO : calculate the downward rounded average of the marks array
+}
+console.log(getAverage([1,2,3,4,5]));
