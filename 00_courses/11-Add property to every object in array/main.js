6c051850b98affdf6a21d200c624a63c3e68c6da
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-10-09 14:15:36 +0200

    11-Add property to every object in array-solution

--- 00_courses/11-Add property to every object in array/main.js
+++ 00_courses/11-Add property to every object in array/main.js
@@ -28,3 +28,21 @@
 // The questions array is already defined for you and is not the same as the one in the example.
 
 // solution:
+const questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+// for (let object in questions) {
+//     Object.assign(questions[object], { usersAnswer: null })
+// }
+// console.log(questions);
+
+for (let object in questions) {
+    questions[object].usersAnswer = null;
+}
+console.log(questions);
