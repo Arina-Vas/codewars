2a0177f1276d3cd581022db127b3705dddef53f3
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-10-09 13:54:42 +0200

    10-Combine objects-solution

--- 00_courses/10_ Combine objects/main.js
+++ 00_courses/10_ Combine objects/main.js
@@ -11,3 +11,40 @@
 // The combine function should be a good citizen, so should not mutate the input objects.
 
 // Solution:
+
+// function combine(obj, obj1) {
+//     const objResult = Object.assign({}, obj);
+//     for (let key1 in obj1) {
+//         if (key1 in objResult) {
+//             objResult[key1] += obj1[key1]
+//         } else {
+//             objResult[key1] = obj1[key1];
+//         }
+//     }
+//     return objResult;
+// }
+// console.log(combine(objA, objB));
+// const objResult = Object.assign({}, objA, objB);
+// console.log(objResult);
+
+const objA = { a: 10, b: 20, c: 30 }
+const objB = { a: 3, c: 6, d: 3 }
+const objC = { a: 5, d: 11, e: 8 }
+const objD = { c: 3 }
+
+function combine(...arguments) {
+    const objectsArray = Array.from(arguments);
+    const objResult = {};
+    for (let i = 0; i < objectsArray.length; i++) {
+        for (let key in objectsArray[i]) {
+            if (key in objResult) {
+                objResult[key] += objectsArray[i][key]
+            } else {
+                objResult[key] = objectsArray[i][key];
+            }
+        }
+    }
+    return objResult;
+}
+console.log(combine(objA, objB, objC, objD));
+
