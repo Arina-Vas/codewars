769da33d08fb2ca5531da57a66b62444f38d1c33
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2024-10-24 19:43:00 +0200

    65-Unique Sum-solution

---
+++ 65-Unique Sum/main.js
@@ -0,0 +1,26 @@
+// Description:
+// Given a list of integers values, your job is to return the sum of the values; however, if the same integer value appears multiple times in the list, you can only count it once in your sum.
+
+// For example:
+
+// [ 1, 2, 3] ==> 6
+
+// [ 1, 3, 8, 1, 8] ==> 12
+
+// [ -1, -1, 5, 2, -7] ==> -1
+
+// [] ==> null
+// Good Luck!
+
+// Solution
+
+function uniqueSum(lst) {
+    if (lst.length === 0) {
+        return null;
+    }
+    let sum = lst.filter((num, i) => {
+        return lst.indexOf(num) === i;
+    }).reduce((a, b) => a + b)
+    return sum;
+}
+console.log(uniqueSum([1, 3, 8, 1, 8]));
